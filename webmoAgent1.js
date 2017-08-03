5601c3b8d8cfab8db44a928331aea4a6a1723df0
岩本 拓也 <iwamoto_takuya_xa@cyberagent.co.jp>
2017-08-03 16:44:31 +0900

    ok

---
+++ webmoAgent1.js
@@ -0,0 +1,86 @@
+0
+00:00:00,000 --> 00:00:01,000
+//Datingwebmo
+doOnce[index] = true;
+player.pauseVideo();
+loadScript('https://cdn.rawgit.com/cidreixd/webmo-library-javascript/master/dist/webmo.min.js',function(e){
+  webmo = new Webmo.http("webmo.local");
+  player.playVideo();
+});
+
+1
+00:00:00,000 --> 00:00:02,000
+webmo.rotateBy(360,360)
+
+2
+00:00:03,500 --> 00:00:06,000
+webmo.rotateBy(40,360)
+
+3
+00:00:6,500 --> 00:00:9,000
+webmo.rotateBy(-95,360)
+
+4
+00:00:9,100 --> 00:00:11,000
+webmo.rotateBy(40,360)
+
+5
+00:00:13,000 --> 00:00:13,300
+webmo.rotateBy(10,360)
+
+6
+00:00:13,310 --> 00:00:13,600
+webmo.rotateBy(-20,360)
+
+7
+00:00:13,610 --> 00:00:13,900
+webmo.rotateBy(20,360)
+
+8
+00:00:13,910 --> 00:00:14,200
+webmo.rotateBy(-20,360)
+
+9
+00:00:14,210 --> 00:00:14,500
+webmo.rotateBy(20,360)
+
+10
+00:00:14,510 --> 00:00:15,000
+webmo.rotateBy(-10,360)
+
+11
+00:00:19,500 --> 00:00:21,000
+webmo.rotateBy(40,360)
+
+12
+00:00:23,500 --> 00:00:25,000
+webmo.rotateBy(-95,360)
+
+13
+00:00:29,500 --> 00:00:31,000
+webmo.rotateBy(40,360)
+
+14
+00:00:42,500 --> 00:00:43,000
+webmo.rotateBy(40,360)
+
+15
+00:00:43,500 --> 00:00:44,500
+webmo.rotateBy(-80,360)
+
+16
+00:00:45,000 --> 00:00:45,500
+webmo.rotateBy(40,360)
+
+17
+00:00:52,000 --> 00:00:53,000
+webmo.rotateBy(40,360)
+
+18
+00:01:04,000 --> 00:01:05,500
+webmo.rotateBy(-80,360)
+
+19
+00:02:14,210 --> 00:02:14,500
+player.seekTo(0,true)
+
